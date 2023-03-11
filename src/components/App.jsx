ec09cc47c195a7ed9019173938d553354330a01c
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-03-11 23:19:38 +0200

    2 step is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,84 @@
 import { Component } from 'react';
-import { Statistics } from 'components/Statistics/Statistics'
-
+import uniqid from 'uniqid';
+import { Statistics } from 'components/Statistics/Statistics';
 export class App extends Component {
+	utils = {
+		total: 0,
+		positivePercentage: 0
+	}
+
 	state = {
 		good: 0,
 		neutral: 0,
-		bad: 0,
-		total: 0,
-		positivePercentage: 0,
+		bad: 0
 	}
 
-	getStatistics(data) {
-		this.setState({
-			...data
-		})
+	handleClick = (e) => {
+		const targetName = e.target.name;
+		this.setState((prevState) => {
+			if (targetName === 'good') {
+				this.countPositiveFeedbackPercentage(prevState.good + 1);
+			} else {
+				this.countPositiveFeedbackPercentage(prevState.good);
+			}
+			return {
+				[targetName]: prevState[targetName] + 1,
+			}
+		});
+		this.countTotalFeedback();
+		
+	}
+
+	countTotalFeedback() {
+		this.utils.total += 1;
+	}
+
+	countPositiveFeedbackPercentage(good) {
+		this.utils.positivePercentage = ((good * 100) / this.utils.total).toFixed(0);
 	}
 
 	render() {
+		const nameBtn = Object.keys(this.state);
+		const statisticsState = Object.entries(this.state);
+		const { total, positivePercentage } = this.utils;
+		
 		return (
 			<>
-				<Statistics
-					good={this.state.good}
-					neutral={this.state.neutral}
-					bad={this.state.bad}
-					total={this.state.total}
-					positivePercentage={this.state.positivePercentage}
-					getStatistics={this.getStatistics}
-				/>
+				{/* <Statistics/> */}
+				<div>
+					<h2>Please leave feedback</h2>
+					<ul>
+						{nameBtn.map(name => {
+								return (
+										<li key={uniqid()}>
+											<button name={name} type='button' onClick={this.handleClick}>{name}</button>
+										</li>
+									)
+							})
+						}
+						
+					</ul>
+				</div>
+
+				<div>
+					<h2>Statistics</h2>
+					<ul>
+						{statisticsState.map(el => {
+								return (
+										<li key={uniqid()}>
+											<p >{el[0]}: {el[1]}</p>
+										</li>
+									)
+							})
+						}
+						<li key={uniqid()}>
+							<p >total: {total}</p>
+						</li>
+						<li key={uniqid()}>
+							<p >Positive feedback: {positivePercentage}%</p>
+						</li>
+					</ul>
+				</div>
 			</>
 		);
 	}
